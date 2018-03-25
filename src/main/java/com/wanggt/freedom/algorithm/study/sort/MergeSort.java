aa53f952ce83e23362624a905ed1f6b9a4823a32
wang465745776 <465745776@qq.com>
2018-03-25 11:39:32 +0800

    1.add a array util 2.add a comparator 3.add sort test 4.add a enum for sort type 5.add merge sort

---
+++ src/main/java/com/wanggt/freedom/algorithm/study/sort/MergeSort.java
@@ -0,0 +1,27 @@
+package com.wanggt.freedom.algorithm.study.sort;
+
+import java.util.Comparator;
+
+import com.wanggt.freedom.algorithm.study.constant.SortType;
+
+/**
+ * 归并排序
+ * @author freedom wang
+ * @date 2018年3月24日下午10:50:22
+ * @version 1.0
+ */
+public class MergeSort implements Sort {
+
+	@Override
+	public int[] sort(int[] primaryArray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] sort(T[] primaryArray, Comparator<T> comparator, SortType sortType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
